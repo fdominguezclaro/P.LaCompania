0a287e705bfc3b2d61ff18076ccdcfa96c6f6e85
fdominguezc <fdominguezc@uc.cl>
2018-05-29 19:27:10 -0400

    contact form v2

--- contact_controller.php
+++ contact_controller.php
@@ -7,12 +7,11 @@ $message = $_POST["message"];
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
-$formcontent="From: $name \n Message: $message";
 $recipient = "fdominguezc@uc.cl";
 $subject="Message sent using your contact form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader);
+mail($recipient, $subject, $message, $mailheader);
+echo "<script type='text/javascript'>alert('Mensaje enviado!');</script>";
 header('Location: index.html');
-echo "Mensaje enviado!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 
 ?>
