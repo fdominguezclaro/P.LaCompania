f5e0a16921c77215a4d2ab20bfa6d7e618b5dac7
fdominguezc <fdominguezc@uc.cl>
2018-05-29 19:35:19 -0400

    contact form v2

--- contact_controller.php
+++ contact_controller.php
@@ -8,10 +8,32 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 $recipient = "fdominguezc@uc.cl";
-$subject="Message sent using your contact form";
+$subject="PLC, from $email";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $message, $mailheader);
 echo "<script type='text/javascript'>alert('Mensaje enviado!');</script>";
 header('Location: index.html');
+
+
+if(isset($_POST['submit']))
+   {
+
+
+   $sendgrid = new SendGrid('app98066099@heroku.com', 'mm4x15xr2217');
+
+  $mail = new SendGrid\Mail();
+   $mail->
+      addTo($recipient)->
+      setFrom('app98066099@heroku.com')->
+      setSubject($subject)->
+      setText($message)->
+      setHtml($mailheader);
+
+   $sendgrid->
+     smtp->
+      send($mail);
+}
+
+
 
 ?>
