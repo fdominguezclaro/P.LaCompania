898b89642db413baa211ee15d5443ec01dbc29a9
fdominguezc <fdominguezc@uc.cl>
2018-05-29 19:43:54 -0400

    contact form v2

--- contact_controller.php
+++ contact_controller.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'vendor/autoload.php';
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
@@ -10,29 +12,22 @@ $message = $_POST["message"];
 $recipient = "fdominguezc@uc.cl";
 $subject="PLC, from $email";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $message, $mailheader);
+
+$sendgrid = new SendGrid('app98066099@heroku.com', 'mm4x15xr2217');
+$mail = new SendGrid\Mail();
+  $mail->
+    addTo($recipient)->
+    setFrom('app98066099@heroku.com')->
+    setSubject($subject)->
+    setText($message)->
+    setHtml($mailheader);
+
+$sendgrid->
+  smtp->
+    send($mail);
+
 echo "<script type='text/javascript'>alert('Mensaje enviado!');</script>";
 header('Location: index.html');
-
-
-if(isset($_POST['submit']))
-   {
-
-
-   $sendgrid = new SendGrid('app98066099@heroku.com', 'mm4x15xr2217');
-
-  $mail = new SendGrid\Mail();
-   $mail->
-      addTo($recipient)->
-      setFrom('app98066099@heroku.com')->
-      setSubject($subject)->
-      setText($message)->
-      setHtml($mailheader);
-
-   $sendgrid->
-     smtp->
-      send($mail);
-}
 
 
 
