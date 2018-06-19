e2325a87afc99e6ba0a8555cb3d588022f3980b1
fdominguezc <fdominguezc@uc.cl>
2018-06-19 01:40:12 -0400

    alert sendgrid

--- contact_controller.php
+++ contact_controller.php
@@ -15,8 +15,8 @@ echo $response->statusCode();
 echo $response->headers();
 echo $response->body();
 
-header('Location: index.html');
 echo "<script type='text/javascript'>alert('Mensaje enviado!');</script>";
+header('Location: index.html');
 
 ?>
 <!--
